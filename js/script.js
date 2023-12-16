8c0132808ff651789199d41d92463021671c332f
Filipe <contatofilipe.sferreira@outlook.com>
2023-12-15 22:25:51 -0300

    refatorando js

--- js/script.js
+++ js/script.js
@@ -1,95 +1,90 @@
 'use strict';
 
-const KEYS = document.getElementsByClassName('keys');
+const NUMBERS = document.getElementsByClassName('keys');
+
 let screen = document.getElementById('screen');
 let resultScreen = document.getElementById('result');
 
-function addKeyOnScreen (event) {
-  let keyboardTarget = event.target.textContent;
+function addKeysOntheScreen(event) {
+  let keyPress = event.target.textContent;
 
-  switch(keyboardTarget) {
+  switch (keyPress) {
+    case '0':
+      screen.value += keyPress;
+      break;
+    case '00':
+      screen.value += keyPress;
+      break;
     case '1':
-      screen.value += keyboardTarget;
+      screen.value += keyPress;
       break;
     case '2':
-      screen.value += keyboardTarget;
+      screen.value += keyPress;
       break;
     case '3':
-      screen.value += keyboardTarget;
+      screen.value += keyPress;
       break;
     case '4':
-      screen.value += keyboardTarget;
+      screen.value += keyPress;
       break;
     case '5':
-      screen.value += keyboardTarget;
+      screen.value += keyPress;
       break;
     case '6':
-      screen.value += keyboardTarget;
+      screen.value += keyPress;
       break;
     case '7':
-      screen.value += keyboardTarget;
+      screen.value += keyPress;
       break;
     case '8':
-      screen.value += keyboardTarget;
+      screen.value += keyPress;
       break;
     case '9':
-      screen.value += keyboardTarget;
-      break;
-    case '0':
-      screen.value += keyboardTarget;
-      break;
-    case '.':
-      if(screen.value === '') {
-        return;
-      } else screen.value += keyboardTarget;
+      screen.value += keyPress;
       break;
     case '+':
-      if(screen.value === '') {
-        return;
-      } else screen.value += keyboardTarget;
+      screen.value += keyPress;
       break;
     case '-':
-      if(screen.value === '') {
-        return;
-      } else screen.value += keyboardTarget;
-      break;
-    case '/':
-      if(screen.value === '') {
-        return;
-      } else screen.value += keyboardTarget;
+      screen.value += keyPress;
       break;
     case '*':
-      if(screen.value === '') {
-        return;
-      } else screen.value += keyboardTarget;
+      screen.value += keyPress;
+      break;
+    case '/':
+      screen.value += keyPress;
       break;
     case '%':
-      if(screen.value === '') {
-        return;
-      } else screen.value += keyboardTarget;
+      screen.value += keyPress;
       break;
-    case '=':
-      if(screen.value === '' || resultScreen.value != '') {
-        resultScreen.value = null;
-        return;
-      } else {
-        resultScreen.value = eval(screen.value);
-        screen.value = null;
-      }
+    case '.':
+      screen.value += keyPress;
       break;
-    case 'C':
-      screen.value = null;
-      resultScreen.value = null;
+    case 'mod':
+      screen.value += keyPress
       break;
-    case 'CE':
-      screen.value = screen.value.slice(0, -1);
-      resultScreen.value = null;
-      break;
-    default:
-      return;
   }
 }
 
-for(let i = 0; i < KEYS.length; i++) {
-  KEYS[i].addEventListener('click', addKeyOnScreen);
+const CLEAR = document.getElementById('clear').addEventListener('click', function clear() {
+  screen.value = null
+  resultScreen.value = null
+});
+
+const CLEAR_ENTRY = document.getElementById('clearEntry').addEventListener('click', function clearEntry() {
+  screen.value = screen.value.slice(0, -1);
+});
+
+const EQUALS = document.getElementById('equals').addEventListener('click', function equals() {
+  try {
+    resultScreen.value = eval(screen.value);
+  } catch (error) {
+    resultScreen.value = error.name;
+  }
+
+  screen.value = null;
+});
+
+for (let i = 0; i < NUMBERS.length; i++) {
+  NUMBERS[i].addEventListener('click', addKeysOntheScreen);
 }
