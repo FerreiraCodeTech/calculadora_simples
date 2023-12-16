ab59acf50223020d5d31c192cf2743f7b0423e59
Filipe <contatofilipe.sferreira@outlook.com>
2023-12-16 15:09:42 -0300

    atualizando o código javascript

--- js/script.js
+++ js/script.js
@@ -8,61 +8,8 @@ let resultScreen = document.getElementById('result');
 function addKeysOntheScreen(event) {
   let keyPress = event.target.textContent;
 
-  switch (keyPress) {
-    case '0':
-      screen.value += keyPress;
-      break;
-    case '00':
-      screen.value += keyPress;
-      break;
-    case '1':
-      screen.value += keyPress;
-      break;
-    case '2':
-      screen.value += keyPress;
-      break;
-    case '3':
-      screen.value += keyPress;
-      break;
-    case '4':
-      screen.value += keyPress;
-      break;
-    case '5':
-      screen.value += keyPress;
-      break;
-    case '6':
-      screen.value += keyPress;
-      break;
-    case '7':
-      screen.value += keyPress;
-      break;
-    case '8':
-      screen.value += keyPress;
-      break;
-    case '9':
-      screen.value += keyPress;
-      break;
-    case '+':
-      screen.value += keyPress;
-      break;
-    case '-':
-      screen.value += keyPress;
-      break;
-    case '*':
-      screen.value += keyPress;
-      break;
-    case '/':
-      screen.value += keyPress;
-      break;
-    case '%':
-      screen.value += keyPress;
-      break;
-    case '.':
-      screen.value += keyPress;
-      break;
-    case 'mod':
-      screen.value += keyPress
-      break;
+  if(keyPress.match(/^[0-9]*$|^\+|-|\*|\/|%|\.|/)) {
+    screen.value += keyPress;
   }
 }
 
